6480bcb7cc60b27707de3ecd71d3276ed13975e8
David Vauzelle <dvauzelle@deastanceservices.fr>
2024-06-06 11:43:20 +0200

    Ajout de la navbar sur toute les pages.

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Projet Portfolio"> 
+    <title>Portfolio David Vauzelle</title>
+    <link rel="stylesheet" href="./style/reset.css">
+    <link rel="stylesheet" href="./style/navbar.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <a href="index.php#accueil">Accueil</a>
+                <div class="liens-navbar">
+                    <a href="./pages/mon-cv.php">Mon CV</a>
+                    <a href="./pages/realisations.php">Mes Réalisations</a>
+                </div>
+            <a href="./pages/contact.php">Contact</a>
+        </nav>    
+    </header>
+
+</body>
+</html>
---
+++ pages/contact.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Projet Portfolio"> 
+    <title>Page Contact</title>
+    <link rel="stylesheet" href="../style/reset.css">
+    <link rel="stylesheet" href="../style/navbar.css">
+</head>
+<body>
+    <header>
+        <!-- Pour récupérer la nav bar -->
+        <?php include '../navbar.php';?> 
+    </header>
+</body>
+</html>
