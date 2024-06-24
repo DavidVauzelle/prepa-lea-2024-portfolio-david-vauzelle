551e6c9286ef02c5ce73c057469c25c950ae47bd
David Vauzelle <dvauzelle@deastanceservices.fr>
2024-06-24 11:10:15 +0200

    Update contact-liste.php pour liste les contacts

--- administration/contact-liste.php
+++ administration/contact-liste.php
@@ -1,49 +1,69 @@
 <?php
-
     // Connexion à la bdd
     include 'connexion.php';
-            
-    // Récupération des contacts pour les afficher sur la page admin contact-liste
-    $sql = "SELECT * FROM `contact`";
-    $resultat = mysqli_query($connexion, $sql);
-
-    // fetch_all --> permet de stocker les données de la requête dans un tableau
-    $resultat = $resultat->fetch_all();
-    print_r($resultat);
-
-    // Si résultat dans le tableau 0, on stock les données du tableau dans une variable
-    if (isset($resultat[0])) {
-        $listeContact = $resultat[0];
-    }
-
 ?>
-<main>
-    <section>
-        <div>
-            <h1>Liste des différents contacts</h1>
-            <?php if (isset($listeContact)) : ?>
-                    
-                <?php foreach($resultat as $listeContact) : ?>
-                    <section>
-                        <?php if (isset($listeContact[0])) : ?>
-                            <article>
-                                <ul>
-                                    <li></li>
-                                    <li></li>
-                                    <li></li>
-                                    <li></li>
-                                    <li></li>
-                                </ul>
-                                <h2><?php echo $listeContact[1]; ?></h2>   
-                                <p><?php echo $listeContact[2]; ?></p>
-                                <img src="<?php echo $listeContact[4]; ?>" 
-                                alt="<?php echo $listeContact[5]; ?>">
-                            </article>
-                        <?php endif; ?>
-                    </section>
-                <?php endforeach; ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Projet Portfolio"> 
+    <title>Page Admin Liste des Contacts</title>
+    <!-- <link rel="stylesheet" href="../style/reset.css"> -->
+    <!-- <link rel="stylesheet" href="../style/nav-footer.css"> -->
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="../index.php#accueil">ACCUEIL</a>
+                <div>
+                    <a href="../mon-cv.php">Mon CV</a>
+                    <a href="../realisations.php">Mes Réalisations</a>
+                </div>
+            <a href="contacts.php">Contacts</a>
+        </nav>
+    </header>
+
+    <main>
+        <?php
+            // Récupération des contacts pour les afficher sur la page admin contact-liste
+            $sql = "SELECT * FROM `contacts`";
+            $resultat = mysqli_query($connexion, $sql);
+
+            // fetch_all --> permet de stocker les données de la requête dans un tableau
+            $resultat = $resultat->fetch_all();
+            // print_r($resultat);
+
+            // Si résultat dans le tableau 0, on stock les données du tableau dans une variable
+            if (isset($resultat[0])) {
+                $listeContact = $resultat[0];
+            }
+        ?>
+        <section>
+            <div>
+                <h1>Liste des différents contacts</h1>
+                <?php if (isset($listeContact)) : ?>
                         
-            <?php endif; ?>
-        </div>
-    </section>    
-</main>
+                    <?php foreach($resultat as $listeContact) : ?>
+                        <section>
+                            <?php if (isset($listeContact[0])) : ?>
+                                <article>
+                                    <ul>
+                                        <li>Identifiant : <?php echo $listeContact[0]; ?></li>
+                                        <li>Nom : <?php echo $listeContact[1]; ?></li>
+                                        <li>e-mail : <?php echo $listeContact[2]; ?></li>
+                                        <li>Téléphone : <?php echo $listeContact[3]; ?></li>
+                                        <li>Message : <?php echo $listeContact[4]; ?></li>
+                                    </ul>
+                                </article>
+                            <?php endif; ?>
+                        </section>
+                    <?php endforeach; ?>
+                            
+                <?php endif; ?>
+            </div>
+        </section>    
+    </main>
+</body>
+</html>
+    
