ae3e53a34a697b543141dd104de259493e07a130
David Vauzelle <dvauzelle@deastanceservices.fr>
2024-06-27 03:30:17 +0200

    Update index

--- index.php
+++ index.php
@@ -33,18 +33,18 @@
         <div class="container-accueil"> 
             <div class="bloc-1-accueil">
                 <section class="presentation-accueil">
-                    <h2>A propos de Moi !</h2>
+                    <h2 class="titre-blue">A propos de Moi !</h2>
                     <div class="container-presentation-accueil">
                         <div class="container-photo-profil">
                             <img class="photo-profil" src="./images/photo-david.jpg" alt="Une photo de profil de David">
                         </div> 
                         <div class="contenu-presentation-accueil">
-                            <p>Bonjour, Moi c'est <span>David</span>, j'ai 32 ans et je suis passionné par <span>l'informatique et les jeux vidéo</span>  depuis l'enfance.</p>
+                            <p>Bonjour, Moi c'est <span class="span-blue">David</span>, j'ai 32 ans et je suis passionné par <span class="span-blue">l'informatique et les jeux vidéo</span>  depuis l'enfance.</p>
                             <p>Dans l'informatique, ce que j'aime, c'est monter, nettoyer et ou formater des PC. J'aime le faire pour moi, mais aussi pour aider les autres.</p>
                             <p>Pour les jeux vidéo, j'aime jouer seul ou à plusieurs, mais aussi streamer afin de partager cette passion avec d'autres.</p>
                             <p>Quand au développement web, ce que j'aime, c'est de partir d'une page blanche, de créer un projet comme un enfant ou adulte qui assemble un set Lego.</p>
                             <p>J'aime le Back End qui permet de faire le lien entre le visible et l'invisible pour l'utilisateur, tel un magicien réalisant un tour de magie.</p>
-                            <p>Actuellement, je prépare le titre <span>"Développeur web/web mobile"</span> afin de devenir <span>Développeur Full Stack.</span></p>
+                            <p>Actuellement, je prépare le titre <span class="span-blue">"Développeur web/web mobile"</span> afin de devenir <span class="span-blue">Développeur Full Stack.</span></p>
                         </div>  
                     </div>
                          
@@ -81,10 +81,10 @@
 
             <div class="bloc-3-accueil">
                 <section class="formations-accueil">
-                    <h2>Formations</h2>
+                    <h2 class="titre-blue">Formations</h2>
                     <div class="container-formations-accueil">
                         <div class="formation-1-accueil">
-                            <h3>Pré-qualification Développeur Web / Web Mobile à <span>l'Ecole Atypique</span></h3>
+                            <h3 class="titre-blue">Pré-qualification Développeur Web / Web Mobile à <span>l'Ecole Atypique</span></h3>
                             <div class="contenu-formations">
                                 <ul>
                                     <li>HTML5</li>
@@ -100,7 +100,7 @@
                             </div>   
                         </div>
                         <div class="formation-2-accueil">   
-                                <h3>Formation Intégrateur Web avec <span>OpenClassrooms</span></h3>
+                                <h3 class="titre-blue">Formation Intégrateur Web avec <span>OpenClassrooms</span></h3>
                                 <ul>
                                     <li>HTML5</li>
                                     <li>CSS3</li>
